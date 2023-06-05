d572054dc10226c7ce07a886d0131effc3bc9cc9
Viktor <Aleksraskin8@gmail.com>
2023-06-05 23:30:20 +0800

    новый проект

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,59 @@
+<nav>
+
+    <div class="nav-container">
+        <a href="/" class="nav-logo" title="Back to Homepage">
+          Главная страница
+        </a>
+        <div class="nav-links">
+            {#each nav as link}
+                {#if link.isShow}
+                 <a href={link.href} class="link">{link.title}</a>
+                {/if}
+            {/each}
+          
+
+        </div>
+    </div>
+  </nav>
+  <div class="container">
+    <!-- Pages will be injected below -->
+    <slot></slot>
+  </div>
+  
+  <style>
+    .container {
+      max-width: 1400px;
+      margin: 50px auto;
+    }
+  </style>
+
+  <!--скрипт-->
+  <script>
+    const nav = [
+    {
+      title: 'О нас',
+      href: '/about',
+      isShow: true,
+    },
+    {
+      title: 'Блог',
+      href: '/blog',
+      isShow: false,
+    },
+    {
+      title: 'Портфолио',
+      href: '/project',
+      isShow: false,
+    },
+    {
+      title: 'Контакты',
+      href: '/contact',
+      isShow: true,
+    },
+    {
+      title: 'temp',
+      href: '/temp',
+      isShow: false,
+    }
+  ]
+</script>
